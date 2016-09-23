35cf1547d62df039790ff75dc0f012964c949f9c
Kris Applesauce <kris@nivenly.com>
2016-09-23 06:50:47 -0600

    super basic example for Kj

--- parser.py
+++ parser.py
@@ -1,27 +1,44 @@
 import sys
+import json
+from collections import OrderedDict
 
-words = {}
-
-
-# Parse a line of text
 def parse_line(line):
     list = line.split()
+    if len(list) == 0:
+        return False
+    global lines
+    global words
+    lines += 1
+
+    #print "Lines: " + str(lines)
     for word in list:
+        print "*",
         if word in words:
             words[word] += 1
         else:
             words[word] = 1
+    return True
 
 
 # Book parsing
 path = "john_dies_at_the_end.txt"
 f = open(path, 'r')
-
+lines = 0
+words = {}
 # Operate on lines instead of the entire file so we don't have a memory overflow
-while True:
+line = f.readline()
+while parse_line(line):
     line = f.readline()
-    parse_line(line)
+
 
 # Output here
+sortedWords = OrderedDict(sorted(words.items(), key=lambda x: x[1]))
+raw = json.dumps(sortedWords, indent=4, separators=(',', ': '))
+text_file = open("Output.json", "w")
+text_file.write(raw)
+text_file.close()
 
-print json.dumps(words, sort_keys=True, indent=4, separators=(',', ': '))
+
+# Lets have some fun
+for word in sortedWords:
+    print word + "  :  " + str(sortedWords[word])
